40181d0eda38908c30459d8de8aa32517bf1294b
dropdel <derrickethornton@protonmail.com>
2020-06-05 05:17:42 +0100

    wrote script for countdown timer in es6, may refactor it a lot later. also, styled it to the center and increased the its font size. #2

---
+++ scripts/clock.js
@@ -0,0 +1,42 @@
+const timer = (endDate) => {
+    const currDate = getTime(endDate); 
+    const timeDifference = diffTime(currDate);
+    const tObj = timeCalc(timeDifference);
+    //dom stuff
+    const clock = document.getElementById("clock");
+    clock.innerHTML = tObj.day + "d " + tObj.hour + "h "
+    + tObj.min + "m " + tObj.sec + "s ";
+    console.log(tObj);
+ }  
+ 
+ const getTime = (endDate) => {
+     const msTime = new Date(endDate).getTime();
+     return msTime;
+ }
+ 
+ const diffTime = (endDate) => {
+     const timeNow = new Date().getTime();
+     const diff = endDate - timeNow;
+     return diff;
+ }
+ 
+ const timeCalc = (diff) => {
+   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+ 
+   class Time {
+       constructor(day, hour, min, sec) {
+           this.day = day;
+           this.hour = hour;
+           this.min = min;
+           this.sec = sec;
+       }
+   }
+   const timeCurr = new Time(days, hours, minutes, seconds);
+ 
+   return timeCurr;
+ } 
+
+ setInterval(timer, 1000, "Nov 18, 2021 00:12:13");
